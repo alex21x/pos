7893cb7459e961557f39b09b38e953c4358950f9
Alexander <fernandezdelacruza@gmail.com>
2021-04-21 12:22:38 -0500

    cambio

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['username'] = 'root';
 $db['default']['password'] = '';
-$db['default']['database'] = 'mundosoft5.0';
+$db['default']['database'] = 'pos';
 $db['default']['dbdriver'] = 'mysqli';
 
 $db['default']['dbprefix'] = '';
